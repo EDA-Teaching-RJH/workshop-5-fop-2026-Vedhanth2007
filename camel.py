516f9f027ab970eac86e2b25952f1e2f508466cb
Vedhanth2007 <vs396@kent.ac.uk>
2026-02-05 10:26:53 +0000

    used .join

--- camel.py
+++ camel.py
@@ -1,1 +1,22 @@
-# Enter your code here
+import re
+captial = []
+word = []
+
+def camel_to_snake():
+    name = input("Enter name here: ")
+    for i in name:
+        if i.isupper():
+            captial.append(i)
+            new = "_" + i
+            new = new.lower()
+            word.append(new)
+        else:
+           word.append(i)
+    p = "".join(word)
+        
+            
+
+    print(p)
+
+camel_to_snake()
+
