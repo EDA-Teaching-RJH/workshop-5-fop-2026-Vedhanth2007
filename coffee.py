2e27b79acc431409030c937eae2da05d471dd087
Vedhanth2007 <vs396@kent.ac.uk>
2026-02-05 10:50:22 +0000

    used if statement

--- coffee.py
+++ coffee.py
@@ -1,1 +1,17 @@
-#Your code goes here
+
+
+num = int(input("Enter the number of coffee's you want: "))
+amount = num * 75
+final = 0
+print("Cost: ",amount)
+while final < amount:
+    coin = int(input("Enter coin here: "))
+    if coin == 50 or coin == 10 or coin == 5 or coin == 20:
+        final = final + coin
+        print("amount deposited:",final)
+    else:
+        print("Coin invalid.")
+        print("amount deposited",final)
+owned = final - amount
+print("Amount owned", owned)
+    
